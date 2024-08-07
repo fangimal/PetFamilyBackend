3510ac50b61d12af2c52f082937ea6e00e0d9884
fangimal <67121530+fangimal@users.noreply.github.com>
2024-08-07 23:23:59 +0500

    до валидации

--- PetFamily.Infrastructure/obj/Debug/net8.0/PetFamily.Infrastructure.AssemblyInfo.cs
+++ PetFamily.Infrastructure/obj/Debug/net8.0/PetFamily.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetFamily.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+267e43aa3f0b7ff6a3b4dde750ac090a45b54244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4bee0a5d925fa6ad716cfe1617501840f87384e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetFamily.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetFamily.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
